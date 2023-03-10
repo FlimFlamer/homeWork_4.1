21e53538f990804b5f8dfecebdd5a21d2719dc4a
FlimFlamer <dronik61@gmail.com>
2023-03-10 22:58:53 +0300

    1

---
+++ src/test/java/com/scypro/recipeapp/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.scypro.recipeapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
